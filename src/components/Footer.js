220c9103035ef77c3ac3a73eaa35a80b3da81e36
beljacob <jacobbelanger11@gmail.com>
2021-11-28 19:24:52 +0200

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+
+        <div>
+            <p className="copyright" style={{ color: 'black', width: '100%', textAlign: 'center', padding: '20px', marginTop: '20px', opacity: 0.8 }}>&copy;2021 - <a href="https://github.com/beljacob" target="_blank" style={{ color: 'black' }}>https://github.com/beljacob</a></p>
+        </div>
+
+    );
+}
+
+export default Footer;
